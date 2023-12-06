79a9a02ca992d19e3b34f337f86595511537ba78
Mahatav Kapoor <87968444+MahatavKapoor@users.noreply.github.com>
2023-12-07 00:21:46 +0530

    Update AdminHome.js

--- Frontend/src/pages/Admin/AdminHome.js
+++ Frontend/src/pages/Admin/AdminHome.js
@@ -14,31 +14,14 @@ const AdminHome = () => {
           <h3>Manage Blood Bank App </h3>
           <hr />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad
-            explicabo animi blanditiis incidunt dicta quia, quibusdam facere
-            corporis! Dolores, reprehenderit cum sed repellat laudantium
-            architecto natus est nostrum accusamus, odio aspernatur minima
-            fugiat quam molestiae nisi. Temporibus impedit dolorem quia.
-            Distinctio modi non excepturi illo odio voluptatum quae nostrum a
-            temporibus sequi! Explicabo, quasi consequatur ad qui quos labore
-            distinctio voluptates alias nostrum ab dicta aspernatur molestias
-            adipisci quibusdam error ipsa. Totam, tenetur dolores eaque tempora
-            officiis deserunt assumenda? Rerum nemo est nihil laudantium
-            necessitatibus. Possimus, voluptatem voluptates blanditiis quas
-            aspernatur, quam, quaerat minus maiores ipsam sint perferendis
-            dolor. Dignissimos voluptatem doloribus sint in quis omnis, atque
-            neque praesentium voluptatum suscipit. Quas esse, accusantium maxime
-            obcaecati iure officiis aperiam minus alias quod cum quos qui
-            voluptatibus, numquam, ad id dolore odit! Minima laudantium sunt,
-            explicabo nesciunt quos voluptatibus qui libero eligendi praesentium
-            debitis obcaecati similique assumenda nobis labore totam dolore
-            perferendis adipisci fugiat quibusdam tempore doloremque voluptatum
-            accusantium. Accusamus, incidunt sequi. Esse sunt officia fuga,
-            officiis saepe tempora repellat suscipit aliquid cupiditate
-            perferendis, asperiores architecto molestiae rem iste eaque
-            molestias reiciendis. Laborum modi asperiores, reprehenderit
-            assumenda numquam, vitae exercitationem illum nesciunt, dolorum
-            deleniti accusamus consequatur id. Necessitatibus dolore ad fugit?
+           Managing rare blood groups efficiently when needed by patients in a timely manner is a critical challenge in the healthcare industry. Existing solutions often lack user-friendliness and accessibility, leading to delays in blood transfusions and potential life-threatening situations. This project aims to address this issue by creating a Hematology Management Website equipped with a User Interface (UI) and User Experience (UX) research tool. This solution will simplify the process of finding rare blood donors, improving patient outcomes and saving lives.
+
+Managing blood donation drives manually can lead to various challenges, including:
+Difficulty in maintaining an updated donor database.
+Inefficient communication between donors and recipients.
+Lack of real-time information on blood availability.
+Limited reach and engagement with potential donors.
+
           </p>
         </div>
       </div>
